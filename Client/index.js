40e6013338d2ab3c3c63199948b6c9d9a4fa8e83
tammydeeplatero <tammyplatero@mail.weber.edu>
2018-06-19 11:43:02 -0600

    working with same code as Brittany

--- Client/index.js
+++ Client/index.js
@@ -1,116 +1,75 @@
-
-//BUILDING THE TRANSACTION
-
-// 1) Creating a private key and using signer module to gets its associated public key
-const{createContext, CryptoFactory} = require('sawtooth-sdk/signing')
+// creating a private key and signer
+const{createContext, CryptoFactory}=require('sawtooth-sdk/signing')
 const context=createContext('secp256k1')
 const privateKey=context.newRandomPrivateKey()
-const signer=new CryptoFactory(context).newSigner(privateKey) 
+const signer=new CryptoFactory(context).newSigner(privateKey)
 
-// 2) Getting Payload
-const cbor = require('cbor')
 const crypto = require('crypto')
 const {protobuf} = require('sawtooth-sdk')
-const TP_FAMILY = 'tp'
-const TpPayload = require ('../Processor/payload')
 
-
-test = new TpPayload (54, "Kewl Stuff", 1.0, 1, 1, 8794561234, "forever");
-
-
-// 3) Encoding Payload - Opaque to Validator
+// creating a hash function
 const _hash = (x) =>
     crypto.createHash('sha512').update(x).digest('hex').toLowerCase()
 
-const payloadBytes = cbor.encode(test)
+// loading payload
+const TP_Family = 'tp'
 
-// 4) Creating the Transaction Header
+const payloadBytes=require('../Processor/payload.js')
+
+//creating a transaction header
 const transactionHeaderBytes = protobuf.TransactionHeader.encode({
-    familyName: TP_FAMILY,
+    familyName: TP_Family,
     familyVersion: '1.0',
-    inputs:[_hash(TP_FAMILY).substring(0,6)],
-    outputs:[_hash(TP_FAMILY).substring(0,6)],
+    // '905b51 is the first 3 bytes of a SHA-512 hash of sampleLicense'
+    inputs:[_hash(TP_Family).substring(0,6)],
+    outputs:[_hash(TP_Family).substring(0,6)],
     signerPublicKey: signer.getPublicKey().asHex(),
-    // In this example, we're signing the batch with the same private key,
-    // but the batch can be signed by another party, in which case, the
-    // public key will need to be associated with that key.
     batcherPublicKey: signer.getPublicKey().asHex(),
-    // In this example, there are no dependencies.  This list should include
-    // an previous transaction header signatures that must be applied for
-    // this transaction to successfully commit.
-    // For example,
-    // dependencies: ['540a6803971d1880ec73a96cb97815a95d374cbad5d865925e5aa0432fcf1931539afe10310c122c5eaae15df61236079abbf4f258889359c4d175516934484a'],
-    dependencies: [],
+    dependencies:[],
     payloadSha512: _hash(payloadBytes)
 }).finish()
 
-// 5) Creating Transaction - Header Signature acts as ID of the transaction.
-const transactionHeaderSignature = signer.sign(transactionHeaderBytes)
-// Contains header signature, bytes, and payload bytes
-const transaction = protobuf.Transaction.create({
+// Creating the transaction
+const transactionHeaderSignature=signer.sign(transactionHeaderBytes)
+
+const transaction= protobuf.Transaction.create({
     header: transactionHeaderBytes,
     headerSignature: transactionHeaderSignature,
     payload: payloadBytes
 })
 
+// Creating the batch header
+const transactions=[transaction]
 
-//OPTIONAL IF
-//The Transactions are batched externally. One or more transactions can be combined into a serialized TransactionList Method OR Serialized as a single Transaction
-//const txnListBytes = protobuf.TransactionList.encode([
-//    transaction1,
-//    transaction2
-//]).finish()
-
-//const txnBytes2 = transaction.finish()
-
-
-
-
-//BUILDING THE BATCH
-//when transactions are ready they are wrapped in a batch. Batches are the ATOMIC unit of change in Sawtooth's state
-
-
-
-// 1) Create the Batch Header - Only needs public key of the signer and list of Transaction ID (in same order as in Batch)
-const transactions = [transaction]
-
-const batchHeaderBytes = protobuf.BatchHeader.encode({
+const batchHeaderBytes=protobuf.BatchHeader.encode({
     signerPublicKey: signer.getPublicKey().asHex(),
-    transactionIds: transactions.map((txn) => txn.headerSignature),
+    transactionIds: transactions.map((txn)=>txn.headerSignature)
 }).finish()
 
-// 2)Create the Batch - Header is signed, Signature acts as the Batch ID
-const batchHeaderSignature = signer.sign(batchHeaderBytes)
-//Contains header signature, bytes, and transactions
-const batch = protobuf.Batch.create ({
-    header: batchHeaderBytes,
+// Creating the batch
+const batchHeaderSignature=signer.sign(batchHeaderBytes)
+
+const batch=protobuf.Batch.create({
+    header:batchHeaderBytes,
     headerSignature: batchHeaderSignature,
     transactions: transactions
 })
 
-// 3) Encode Batch(es) in a BatchList - Multiple Batches can be submitted in one BatchList. Batchlist IS NOT ATOMIC, meaning, 
-//Batches from other clients can be interleaved with your own
-const batchListBytes = protobuf.BatchList.encode({
+// Encoding the batch(es) in a batchlist
+const batchListBytes=protobuf.BatchList.encode({
     batches: [batch]
 }).finish()
 
-
-
-//SUBMITTING BATCHES TO THE VALIDATOR
-//Recommended to submit to validator via REST API. Communicate using HTTP/JSON standards
-//Simply send a POST request to the /batches endpoint, with a “Content-Type” header of “application/octet-stream”, 
-//and the body as a serialized BatchList.
-
-
-
-const request = require('request')
+// Submitting the batches to the validator
+const request=require('request') // tuna example uses 'jquery' instead of 'request'
 
 request.post({
-    url: 'https://tammynew.echidna.site/sawtooth/batches',
+    url: 'https://tammynew.echidna.site/sawtooth/batches', //this will change
     body: batchListBytes,
-    headers: {'Content-Type': 'application/octet-stream'}
-}, (err, response) => {
-    if (err) return console.log(err)
+    headers:{'Content-Type': 'application/octet-stream'}
+}, (err,response) => {
+    if(err) return console.log(err)
     console.log(response.body)
 })
+
 
--- Processor/handler.js
+++ Processor/handler.js
@@ -1,33 +1,138 @@
-'use strict'
+const { TransactionHandler } = require('sawtooth-sdk/processor/handler')
+const { InvalidTransaction, InternalError} = require('sawtooth-sdk/processor/exceptions')
+const crypto = require('crypto')
+const cbor = require('cbor')
 
-const TpPayload = require('./payload')
-
-const { TP_NAMESPACE, TP_FAMILY, TpState } = require('./state')
-
-const { TransactionHandler } = require('sawtooth-sdk/processor/handler');
+const _hash = (x) =>
+    crypto.createHash('sha512').update(x).digest('hex').toLowerCase()
 
 const _decodeCbor = buffer =>
     new Promise((resolve,reject) =>
         cbor.decodeFirst(buffer, (err, obj) => (err ? reject(err): resolve(obj)))
     )
 
-const { TransactionHandler } = require('sawtooth-sdk/processor/handler')
+const _toInternalError = (err) => {
+        let message = (err.message) ? err.message : err
+        throw new InternalError(message)
+    }
+    
+const _decodeData = (data) => {
+        return data.toString().split(",")
+    }
+    
+const _encodeData = (data) => {
+        return Buffer.from(data.join())
+      }
+
+const TP_FAMILY = 'Licenses'
+const TP_NAMESPACE = _hash(TP_FAMILY).substring(0,6)
+const TP_VERSION = '1.0'
+
+const _countLicenses = (context, address, newOrder) => (possibleAddressValues) =>{
+    let stateValueRep = possibleAddressValues[address]
+
+    let data
+    if (stateValueRep && stateValueRep.length > 0) {
+        data = _decodeData(stateValueRep)
+        if(data){
+            throw new InvalidTransaction('There is already an order for this order id')
+        }
+    }
+    if (!data){
+        data = {}
+        data=newOrder
+    }
+    console.log('Order ' + newOrder[0] + ' for customer ' + newOrder[1] + ' has been submitted')
+    
+    return _setEntry(context, address, data)
+  
+}
+
+
+const _setEntry=(context, address, data) => {
+    let entries = {
+        [address]: _encodeData(data)
+
+    }
+    return context.setState(entries)
+}
 
 class TPHandler extends TransactionHandler {
-    constructor () {
-        super(TP_FAMILY, ['1.0'], [TP_NAMESPACE])
+    constructor(){
+        super(TP_FAMILY,[TP_VERSION],[TP_NAMESPACE])
     }
-
-    apply (transactionProcessRequest, context) {
-        payload = TpPayload.fromBytes(transactionProcessRequest.payload);
-        tpState = new TpState(context);
-        header = transactionProcessRequest.header;
-
+    apply (transactionProcessRequest, context){
         return _decodeCbor(transactionProcessRequest.payload)
         .catch(_toInternalError)
-        .then((update) => {
-            console.log('Payload Info here')
-        }) 
+        .then((update)=>{
+            let order_id = update.Order_ID
+            if(!order_id) {
+                throw new InvalidTransaction('Order ID is Required')
+            }
+            let organization_id = update.Organization_ID
+            if (!organization_id) {
+                throw new InvalidTransaction('Organization ID is required')
+            }
+            let lineNumber = update.LineNumber
+            if (!lineNumber) {
+                throw new InvalidTransaction('LineNumber is required')
+            }
+            let product_id = update.Product_ID
+            if (!product_id) {
+                throw new InvalidTransaction('Product ID is required')
+            }
+            let sku=update.Sku
+            if (!sku) {
+                throw new InvalidTransaction('Sku is required')
+            }     
+            let start_date = update.Start_Date
+            if (!start_date) {
+                throw new InvalidTransaction('Start date is required')
+            }     
+            let end_date=update.End_Date
+            if (!end_date) {
+                throw new InvalidTransaction('End date is required')
+            }
+            let quantity = update.Quantity
+            if(!quantity){
+                throw new InvalidTransaction('Quantity is required')
+            }
+            let enabled = update.Enabled
+            if (!enabled){
+                throw new InvalidTransaction('Enabled is required')
+            }
+            if (enabled != 'True' && enabled != 'False'){
+                throw new InvalidTransaction('Enabled must be a boolean')
+            }
+            let type_id=update.Type_ID 
+            if (!type_id){
+                throw new InvalidTransaction('Type ID is required')
+            }
+            let is_perpetual = update.Is_Perpetual
+            if (!is_perpetual){
+                throw new InvalidTransaction('Is Perpetual is required')
+            }
+            if (is_perpetual != 'True' && is_perpetual != 'False'){
+                throw new InvalidTransaction('Is Perpetual must be a boolean')
+            }
+
+            let address = TP_NAMESPACE + _hash(organization_id).slice(-21) + _hash(product_id).slice(-21) + _hash(order_id).slice(-22) 
+
+            let newOrder = [order_id, organization_id, lineNumber, product_id, sku, start_date, end_date, quantity, enabled, type_id, is_perpetual]
+
+            let getPromise = context.getState([address])
+
+            let actionPromise = getPromise.then(
+                _countLicenses(context, address, newOrder)
+            )
+            return actionPromise.then(addresses=>{
+                if (addresses.length===0){
+                    throw new InternalError('State error!')
+                }
+                console.log('The blockchain was updated')
+            })
+        })
+
     }
 }
 
